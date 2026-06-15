6504d5f24e92a32515bbf4d5cd7e90d55ec5c778
edufigura <edu.figueroas@duocuc.cl>
2026-06-14 20:24:24 -0400

    Fixeo con el BFF

--- src/main/java/DuocQuin/Usuarios/utils/SecurityConfig.java
+++ src/main/java/DuocQuin/Usuarios/utils/SecurityConfig.java
@@ -31,6 +31,7 @@ public class SecurityConfig {
                 .requestMatchers(org.springframework.http.HttpMethod.OPTIONS, "/**").permitAll()
                 .requestMatchers("/auth/**").permitAll()
                 .requestMatchers(org.springframework.http.HttpMethod.POST, "/api/usuarios").permitAll()
+                .requestMatchers("/error").permitAll()
                 .requestMatchers("/api/usuarios/**").authenticated()
                 .anyRequest().authenticated()
             )
